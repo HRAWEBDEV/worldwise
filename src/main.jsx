917f7f9a980b9f57022c0a2176af316b65f8f76d
hamidwise1 <hrawebdev@gmail.com>
2023-09-08 12:44:08 +0430

    create app scaffold with vite

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+const rootEl = document.getElementById('root');
+const reactRoot = createRoot(rootEl);
+
+reactRoot.render(
+ <StrictMode>
+  <App />
+ </StrictMode>
+);
