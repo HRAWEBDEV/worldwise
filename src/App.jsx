72ab26e415e6db559db5a87bec826ce3eb372098
hamidwise1 <hrawebdev@gmail.com>
2023-09-08 13:46:05 +0430

    add main routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import Pricing from './pages/Pricing';
 import Product from './pages/Product';
+import NotFound from './pages/NotFound';
 
 const App = () => {
  return (
   <BrowserRouter>
    <Routes>
+    <Route path='/' element={<HomePage />} />
     <Route path='product' element={<Product />} />
+    <Route path='pricing' element={<Pricing />} />
+    <Route path='*' element={<NotFound />} />
    </Routes>
   </BrowserRouter>
  );
