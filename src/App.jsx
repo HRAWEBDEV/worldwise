22d9b726792f5e5b0a1fc638558980f5042f73a3
hamidwise1 <hrawebdev@gmail.com>
2023-09-13 14:22:15 +0430

    add main components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import Pricing from './pages/Pricing';
-import Product from './pages/Product';
-import NotFound from './pages/NotFound';
+import '../index.css';
 
 const App = () => {
  return (
   <BrowserRouter>
    <Routes>
     <Route path='/' element={<HomePage />} />
-    <Route path='product' element={<Product />} />
-    <Route path='pricing' element={<Pricing />} />
-    <Route path='*' element={<NotFound />} />
    </Routes>
   </BrowserRouter>
  );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,20 @@
-import Navigation from '../components/Navigation';
+import styles from "./Homepage.module.css";
 
-const HomePage = () => {
- return (
-  <div>
-   <Navigation />
-   homepage
-  </div>
- );
-};
-
-export default HomePage;
+export default function Homepage() {
+  return (
+    <main className={styles.homepage}>
+      <section>
+        <h1>
+          You travel the world.
+          <br />
+          WorldWise keeps track of your adventures.
+        </h1>
+        <h2>
+          A world map that tracks your footsteps into every city you can think
+          of. Never forget your wonderful experiences, and show your friends how
+          you have wandered the world.
+        </h2>
+      </section>
+    </main>
+  );
+}
