92c5833a97aadf93d234a0a157788438c9d71f66
hamidwise1 <hrawebdev@gmail.com>
2023-09-13 15:39:00 +0430

    add pages to react router routess

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import Pricing from './pages/Pricing';
+import Product from './pages/Product';
+import NotFound from './pages/PageNotFound';
+import Login from './pages/Login';
 import '../index.css';
 
 const App = () => {
@@ -7,6 +11,10 @@ const App = () => {
   <BrowserRouter>
    <Routes>
     <Route path='/' element={<HomePage />} />
+    <Route path='/pricing' element={<Pricing />} />
+    <Route path='/product' element={<Product />} />
+    <Route path='/login' element={<Login />} />
+    <Route path='*' element={<NotFound />} />
    </Routes>
   </BrowserRouter>
  );
