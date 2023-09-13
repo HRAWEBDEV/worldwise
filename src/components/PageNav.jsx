b6b8b494535f1339ff3176afbd2720ac3da99e6b
hamidwise1 <hrawebdev@gmail.com>
2023-09-13 15:33:14 +0430

    add navigation tot logo

---
+++ src/components/PageNav.jsx
@@ -0,0 +1,24 @@
+import styles from './PageNav.module.css';
+import { NavLink } from 'react-router-dom';
+import Logo from './Logo';
+
+const PageNav = () => {
+ return (
+  <nav className={styles.nav}>
+   <Logo />
+   <ul>
+    <li>
+     <NavLink to='/pricing'>pricing</NavLink>
+    </li>
+    <li>
+     <NavLink to='/product'>product</NavLink>
+    </li>
+    <li>
+     <NavLink to='/login'>login</NavLink>
+    </li>
+   </ul>
+  </nav>
+ );
+};
+
+export default PageNav;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,10 @@
 import styles from './Homepage.module.css';
 import { Link } from 'react-router-dom';
+import PageNav from '../components/PageNav';
 export default function Homepage() {
  return (
   <main className={styles.homepage}>
+   <PageNav />
    <section>
     <h1>
      You travel the world.
