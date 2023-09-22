ce63b2ae7a6bb3caba81a99e953ffd47025e8afe
hamidwise1 <hrawebdev@gmail.com>
2023-09-22 15:32:43 +0330

    add code splitting with react lazy function

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
+import { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import CitiesProvider from './components/CitiesProvider';
-import HomePage from './pages/HomePage';
-import Pricing from './pages/Pricing';
-import Product from './pages/Product';
-import NotFound from './pages/PageNotFound';
-import Login from './pages/Login';
+import './index.css';
 import AppLayout from './pages/AppLayout';
-import CityList from './components/CityList';
 import CountryList from './components/CountryList';
+import CityList from './components/CityList';
+import CitiesProvider from './components/CitiesProvider';
+import NotFound from './pages/PageNotFound';
+import SpinnerFullPage from './components/SpinnerFullPage';
+
 import City from './components/City';
 import Form from './components/Form';
-import './index.css';
+
+const HomePage = lazy(() => import('./pages/Homepage'));
+const Pricing = lazy(() => import('./pages/Pricing'));
+const Product = lazy(() => import('./pages/Product'));
+const Login = lazy(() => import('./pages/Login'));
 
 const App = () => {
  return (
   <CitiesProvider>
    <BrowserRouter>
-    <Routes>
-     <Route path='/' element={<HomePage />} />
-     <Route path='/pricing' element={<Pricing />} />
-     <Route path='/product' element={<Product />} />
-     <Route path='/login' element={<Login />} />
-     <Route path='/app' element={<AppLayout />}>
-      <Route index element={<Navigate replace to='cities' />} />
-      <Route path='cities' element={<CityList />} />
-      <Route path='cities/:id' element={<City />} />
-      <Route path='countries' element={<CountryList />} />
-      <Route path='forms' element={<Form />} />
-     </Route>
-     <Route path='*' element={<NotFound />} />
-    </Routes>
+    <Suspense fallback={<SpinnerFullPage />}>
+     <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/pricing' element={<Pricing />} />
+      <Route path='/product' element={<Product />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/app' element={<AppLayout />}>
+       <Route index element={<Navigate replace to='cities' />} />
+       <Route path='cities' element={<CityList />} />
+       <Route path='cities/:id' element={<City />} />
+       <Route path='countries' element={<CountryList />} />
+       <Route path='forms' element={<Form />} />
+      </Route>
+      <Route path='*' element={<NotFound />} />
+     </Routes>
+    </Suspense>
    </BrowserRouter>
   </CitiesProvider>
  );
